1df9c9d78c7b72b89c53af6c826bb6bff911b9d9
0371991552 <mohammadrezamajma82@gmail.com>
2025-10-14 08:07:02 +0330

    Tue 10/14/2025  8:07:01.87

--- obj/Debug/net8.0/MyGreenlightAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyGreenlightAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGreenlightAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c0252622ecdb426307dd2ec56aabfe098d9509")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGreenlightAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGreenlightAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
